67fa10af125ebb800de0237dbeb3a034c34e4c47
Choi Junyoung <nueeeng@gmail.com>
2023-04-12 10:39:38 +0900

    main page 변경

--- tweet/urls.py
+++ tweet/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
-    path('tweet/', views.tweet, name='tweet'),
+    path('', views.main, name='main'), # 여기가 사이트 진입 url path 같습니다
     path('main/', views.main, name='main'),
+    path('tweet/', views.tweet, name='tweet'),
     path('tweet/delete/<int:id>', views.delete_tweet, name='delete-tweet'),
     path('tweet/edit/<int:id>', views.edit_tweet, name='edit-tweet'),
     path('tweet/update/<int:id>', views.update_tweet, name='update-tweet'),
