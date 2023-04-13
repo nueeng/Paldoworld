20a96350747f46211ea7a90839b4c495d3a85ea8
Choi Jinkyu <kyuparfum@naver.com>
2023-04-13 10:56:44 +0900

    프로필 이름변경

--- static/js/main.js
+++ static/js/main.js
@@ -1,20 +1,33 @@
-const link_a = document.querySelectorAll('.link a')
-let close = true
-console.log(link_a)
+    const link_a = document.querySelectorAll('.link a');
+    let close = true;
 
-link_a.forEach(function (t, i) {
-    t.addEventListener('click', function (e) {
-        e.preventDefault()
-        close = true
-        for (const n of link_a) { n.classList.remove('active1') }
-        console.log('----')
-        if (close == true) {
-            link_a[i].classList.add('active1')
-            console.log(link_a[i])
-            close = false
-        } else {
-            close = true
-        }
+    // Check if there is a value stored in localStorage for the button
+    if (localStorage.getItem('activeButton')) {
+        // Get the index of the active button from localStorage
+        const activeButtonIndex = parseInt(localStorage.getItem('activeButton'));
 
-    })
-})
+        // Add the active class to the button
+        link_a[activeButtonIndex].classList.add('active1');
+        close = false;
+    }
+
+    link_a.forEach(function (t, i) {
+        t.addEventListener('click', function (e) {
+            // e.preventDefault();
+            close = true;
+
+            // Remove the active class from all buttons
+            for (const n of link_a) { n.classList.remove('active1'); }
+
+            if (close == true) {
+                // Add the active class to the clicked button
+                link_a[i].classList.add('active1');
+                close = false;
+
+                // Save the index of the clicked button to localStorage
+                localStorage.setItem('activeButton', i);
+            } else {
+                close = true;
+            }
+        });
+    });
