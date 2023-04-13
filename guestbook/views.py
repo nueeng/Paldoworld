7b597d7766e3e3d406fb1dafa419666a8dec7252
madonghwi <ehd0831@naver.com>
2023-04-13 10:57:17 +0900

    방명록

--- guestbook/views.py
+++ guestbook/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render, redirect, get_object_or_404
+from django.http import Http404
 from .models import GuestbookModel, UserModel
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from .models import GuestbookModel
 from user.models import UserModel
+from django.utils import timezone
 
 @login_required
 def guestbook(request):
@@ -14,11 +16,14 @@ def guestbook(request):
         user = request.user
         my_guestbook = GuestbookModel()
         my_guestbook.author = user
-        my_guestbook.author_nickname = user.username
+        my_guestbook.author_nickname_id = user.id
         my_guestbook.content = request.POST.get('my-content', '')
-        my_guestbook.created_at = request.POST.get('-created_at')
+        my_guestbook.created_at = timezone.now()  # 유효한 값을 할당합니다.
         my_guestbook.save()
-        return render('/guestbook')
+        return redirect('guestbook')
+
+
+
 
 @login_required
 def edit_guestbook(request, id):
@@ -39,14 +44,30 @@ def delete_guestbook(request, id):
     my_guestbook.delete()
     return redirect('guestbook')
 
+# ...
+
+from django.http import Http404
 
 @login_required
-def view_guestbook(request, username):
+def view_guestbook(request, author):
     try:
-        owner = UserModel.objects.get(username=username)
+        owner = UserModel.objects.get(nickname=author)
     except UserModel.DoesNotExist:
-        return redirect('login')
+        raise Http404()
 
-    guestbook_entries = GuestbookModel.objects.filter(author=owner)
-    context = {'owner_username': owner.username, 'guestbook': guestbook_entries}
-    return render(request, 'guestbook/guestbook.html', context)
+    if request.method == 'POST':
+        content = request.POST.get('my-content', '')
+        author_nickname = request.user.nickname
+        guestbook = GuestbookModel.objects.create(content=content, author=request.user, author_nickname=author_nickname)
+        owner.guestbook.add(guestbook)
+
+    guestbook = owner.guestbook.all().order_by('-created_at')
+    context = {
+        'owner': owner,
+        'guestbook': guestbook,
+    }
+    return render(request, 'guestbook/guestbook.html', context=context)
+
+
+
+
