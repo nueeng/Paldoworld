a326d3076a1a6fb84d3ab35a1b71adc0b5ee77dd
Choi Jinkyu <kyuparfum@naver.com>
2023-04-14 17:11:24 +0900

    슬랙에 올린 수정내용작업물

--- static/js/main.js
+++ static/js/main.js
@@ -1,4 +1,5 @@
 const link_a = document.querySelectorAll('.link a');
+const link_a1 = document.querySelectorAll('.link1 a');
 let close = true;
 
 if (localStorage.getItem('activeButton')) {
@@ -27,3 +28,30 @@ link_a.forEach(function (t, i) {
         }
     });
 });
+if (localStorage.getItem('activeButton')) {
+    const activeButtonIndex = parseInt(localStorage.getItem('activeButton'));
+
+    link_a1[activeButtonIndex].classList.add('active1');
+    close = true;
+}
+
+
+link_a1.forEach(function (t, i) {
+    t.addEventListener('click', function (e) {
+        // e.preventDefault();
+        close = true;
+
+        for (const n of link_a) {
+            n.classList.remove('active1');
+        }
+        if (close == true) {
+            link_a[i].classList.add('active1');
+            close = false;
+
+            localStorage.setItem('activeButton', i);
+        } else {
+            close = true;
+        }
+    });
+});
+
