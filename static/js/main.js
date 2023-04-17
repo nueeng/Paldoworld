9838fd97bba2a728fbeb6c1d3f2c9aa3fe898879
Choi Jinkyu <kyuparfum@naver.com>
2023-04-17 09:17:02 +0900

    스크롤, 링크박스 수정

--- static/js/main.js
+++ static/js/main.js
@@ -1,57 +1,41 @@
 const link_a = document.querySelectorAll('.link a');
-const link_a1 = document.querySelectorAll('.link1 a');
 let close = true;
 
 if (localStorage.getItem('activeButton')) {
     const activeButtonIndex = parseInt(localStorage.getItem('activeButton'));
 
     link_a[activeButtonIndex].classList.add('active1');
-    close = true;
+    close = false;
 }
-
-
+for (const n of link_a) {
+    n.classList.remove('active1');
+}
 link_a.forEach(function (t, i) {
     t.addEventListener('click', function (e) {
-        // e.preventDefault();
         close = true;
 
         for (const n of link_a) {
             n.classList.remove('active1');
         }
+
         if (close == true) {
             link_a[i].classList.add('active1');
             close = false;
-
-            localStorage.setItem('activeButton', i);
+            localStorage.setItem('activeLink', t.href); // Store the clicked link's href value
         } else {
             close = true;
         }
     });
 });
-if (localStorage.getItem('activeButton')) {
-    const activeButtonIndex = parseInt(localStorage.getItem('activeButton'));
 
-    link_a1[activeButtonIndex].classList.add('active1');
-    close = true;
+// Check if there is a value stored in localStorage for the active link
+if (localStorage.getItem('activeLink')) {
+    const activeLink = localStorage.getItem('activeLink');
+
+    // Find the link with the stored href value and add the active class to it
+    link_a.forEach((link) => {
+        if (link.href === activeLink) {
+            link.classList.add('active1');
+        }
+    });
 }
-
-
-link_a1.forEach(function (t, i) {
-    t.addEventListener('click', function (e) {
-        // e.preventDefault();
-        close = true;
-
-        for (const n of link_a) {
-            n.classList.remove('active1');
-        }
-        if (close == true) {
-            link_a[i].classList.add('active1');
-            close = false;
-
-            localStorage.setItem('activeButton', i);
-        } else {
-            close = true;
-        }
-    });
-});
-
